69dde98d41c1ad7c9fa843d219cb2ae66ab06cdc
Keith Carter <keith.carter@yahoo.com>
2016-02-14 13:33:24 -0800

    rounding and another data point

--- scrape.rb
+++ scrape.rb
@@ -24,20 +24,22 @@ data['resultSets']['rowSet'].each_with_index do |player,i|
   name = player[1]
   paintMakes = clean(player[5])+clean(player[8])
   paintAttempts = clean(player[6])+clean(player[9])
-  paintPercent = paintMakes > 1 ? paintMakes.to_f / paintAttempts : nil
+  paintPercent = paintMakes > 1 ? 100 * paintMakes.to_f / paintAttempts : nil
 
   midMakes = clean(player[11])
   midAttempts = clean(player[12])
-  midPercent = midMakes > 1 ? midMakes.to_f / midAttempts : nil
+  midPercent = midMakes > 1 ? 100 * midMakes.to_f / midAttempts : nil
 
   threeAttempts = clean(player[15])+clean(player[18])+clean(player[21])
   threeMakes = clean(player[14])+clean(player[17])+clean(player[20])
-  threePercent = threeMakes > 1 ? threeMakes.to_f / threeAttempts : nil
+  threePercent = threeMakes > 1 ? 100 * threeMakes.to_f / threeAttempts : nil
 
   # (FG + 0.5 * 3P) / FGA
   if midAttempts > 0 || threeAttempts > 0
-  	outsidePaintEPercent = (midMakes + threeMakes * 1.5).to_f / (midAttempts + threeAttempts) 
+  	outsidePaintPercent = 100 * (midMakes + threeMakes).to_f / (midAttempts + threeAttempts) 
+  	outsidePaintEPercent = 100 * (midMakes + threeMakes * 1.5).to_f / (midAttempts + threeAttempts) 
   else
+  	outsidePaintPercent = nil
     outsidePaintEPercent = nil
   end
 
@@ -52,10 +54,11 @@ data['resultSets']['rowSet'].each_with_index do |player,i|
   	threeMakes,
   	threeAttempts,
   	threePercent,
+  	outsidePaintPercent,
   	outsidePaintEPercent
   ])
 end
 
-massagedData.reject! { |player| player[5] + player[8] < 300 }
+massagedData.reject! { |player| player[5] + player[8] < 250 }
 
 massagedData.sort! { |x,y| x[10] <=> y[10] }
